52dcaa9a5810764aeb37c5817813e5134985b206
Robbie08 <roberinho08@gmail.com>
2019-12-18 02:32:41 -0800

    Add Project: Architecture setup(basic)

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import Main from './Components/Main'
+import './styles/stylesheet.css'
+
+
+
+// will render our code to the browser
+ReactDOM.render(<Main/>, document.getElementById('root'));
